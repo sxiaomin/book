d00d73de129d7bd7c02b5f61f3504aaf2df1be4a
sxiaomin <m1371786385151_1@163.com>
2018-06-15 20:34:29 +0800

    开发环境搭建完成

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,30 @@ var gulp = require('gulp');
 var server = require('gulp-webserver');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sequence = require('gulp-sequence');
 // 引入内置模块
 var url = require('url');
 var path = require('path');
 var fs = require('fs');
 
+// 开发---编译sass 添加前缀
+gulp.task('devSass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            borwsers: ['last 2 versions', 'Android > 4.0']
+        }))
+        .pipe(gulp.dest('src/css'))
+})
+
+// 开发---监听sass
+gulp.task('devWatch', function() {
+    return gulp.watch('src/scss/*.scss', ['devSass'])
+})
+
 //起服务
-gulp.task('server', function() {
-    gulp.src('src')
+gulp.task('server', ['devSass'], function() {
+    return gulp.src('src')
         .pipe(server({
             port: 8181,
             middleware: function(req, res, next) {
@@ -22,4 +38,9 @@ gulp.task('server', function() {
                 res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
             }
         }))
+})
+
+// 开发环境
+gulp.task('dev', function(callback) {
+    sequence('devSass', 'devWatch', 'server', callback)
 })
